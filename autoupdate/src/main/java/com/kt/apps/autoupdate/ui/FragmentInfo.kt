3ba3802b9cc482988665a48cc711f954308e2df4
tuandv4 <dvt1405@gmail.com>
2023-07-24 11:50:32 +0700

    Get info from remote

--- autoupdate/src/main/java/com/kt/apps/autoupdate/ui/FragmentInfo.kt
+++ autoupdate/src/main/java/com/kt/apps/autoupdate/ui/FragmentInfo.kt
@@ -3,14 +3,19 @@ package com.kt.apps.autoupdate.ui
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.kt.apps.autoupdate.R
 import com.kt.apps.autoupdate.databinding.FragmentInfoBinding
 import com.kt.apps.core.base.BaseFragment
 import com.kt.apps.core.base.DataState
 import com.kt.apps.core.base.leanback.BrowseFrameLayout
 import com.kt.apps.core.base.leanback.BrowseSupportFragment
+import org.json.JSONObject
 import javax.inject.Inject
 
 class FragmentInfo : BaseFragment<FragmentInfoBinding>(), BrowseSupportFragment.MainFragmentAdapterProvider {
@@ -88,6 +93,35 @@ class FragmentInfo : BaseFragment<FragmentInfoBinding>(), BrowseSupportFragment.
                 }
             }
         }
+        getFbZlGroupsLink()
+    }
+
+    private fun getFbZlGroupsLink(retryTimes: Int = 3) {
+        Firebase.remoteConfig
+            .fetch()
+            .addOnSuccessListener {
+                try {
+                    val jsonGroups = JSONObject(Firebase.remoteConfig.getString("user_groups"))
+                    jsonGroups.optString("facebook").takeIf {
+                        it.isNotEmpty() && it.isNotBlank()
+                    }?.let {
+                        binding.fbGroupLink.text = it
+                    }
+                    jsonGroups.optString("zalo").takeIf {
+                        it.isNotEmpty() && it.isNotBlank()
+                    }?.let {
+                        binding.zaloGroupLink.text = it
+                    }
+                } catch (_: Exception) {
+                }
+            }
+            .addOnFailureListener {
+                if (retryTimes > 0) {
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        getFbZlGroupsLink(retryTimes - 1)
+                    }, 2000)
+                }
+            }
     }
 
     override fun getMainFragmentAdapter(): BrowseSupportFragment.MainFragmentAdapter<*> {
