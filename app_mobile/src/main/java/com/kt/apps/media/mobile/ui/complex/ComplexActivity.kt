fb73dcb4af86a22e36f801fc7839d7c8a0e034e8
anntm2 <anntm2@vng.com.vn>
2023-10-11 10:32:37 +0700

    Fix show hide system UI on Android DVD

--- app_mobile/src/main/java/com/kt/apps/media/mobile/ui/complex/ComplexActivity.kt
+++ app_mobile/src/main/java/com/kt/apps/media/mobile/ui/complex/ComplexActivity.kt
@@ -148,10 +148,11 @@ class ComplexActivity : BaseActivity<ActivityComplexBinding>() {
                 }
             } else {
                 window.decorView.apply {
-                    systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    systemUiVisibility =
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 }
             }
-            actionBar?.hide()
+
         } else {
             WindowCompat.setDecorFitsSystemWindows(window, true)
             WindowInsetsControllerCompat(window, binding.root).let {
