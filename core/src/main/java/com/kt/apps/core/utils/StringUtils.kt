9d4c4b8ac5618e2638c89280fa3059ea8767cc4f
tuandv4 <dvt1405@gmail.com>
2023-11-25 19:59:22 +0700

    Fix expand url with host and user agent
    Add test source

--- core/src/main/java/com/kt/apps/core/utils/StringUtils.kt
+++ core/src/main/java/com/kt/apps/core/utils/StringUtils.kt
@@ -1,12 +1,16 @@
 package com.kt.apps.core.utils
 
+import android.net.Uri
 import com.kt.apps.core.Constants
 import com.kt.apps.core.logging.Logger
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import java.net.HttpURLConnection
 import java.net.URL
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 import java.util.regex.Pattern
 
 val listShortLink by lazy {
@@ -18,7 +22,8 @@ val listShortLink by lazy {
         "tinyurl.com",
         "tiny.cc",
         "bit.do",
-        "cocc.me"
+        "cocc.me",
+        "hqth.me"
     )
 }
 fun String.formatDateTime(
@@ -72,18 +77,41 @@ fun String.isShortLink(): Boolean {
     }
 }
 
-fun String.expandUrl(): String {
+fun String.expandUrl(
+    props: Map<String, String>? = null,
+): String {
     var conn: HttpURLConnection? = null
     try {
         val inputURL = URL(this)
         conn = inputURL.openConnection() as HttpURLConnection?
+        conn?.addRequestProperty(
+            "User-Agent", props?.get("user-agent") ?: Constants.USER_AGENT,
+        )
+        conn?.addRequestProperty(
+            "Host", Uri.parse(this).host
+        )
+        conn?.requestMethod = "HEAD"
         conn?.instanceFollowRedirects = false
-        while (conn!!.responseCode / 100 == 3) {
-            val location: String = conn.getHeaderField("location");
+        var responseCode = conn?.responseCode ?: 0
+        var maxRedirect = 20
+        while (responseCode / 100 == 3) {
+            val location: String = conn?.getHeaderField("Location") ?: break
+            maxRedirect--
+            if (maxRedirect < 0) {
+                break
+            }
             conn = URL(location).openConnection() as HttpURLConnection?
+            conn?.instanceFollowRedirects = false
+            conn?.addRequestProperty(
+                "User-Agent", props?.get("user-agent") ?: Constants.USER_AGENT,
+            )
+            conn?.addRequestProperty(
+                "Host", Uri.parse(location).host
+            )
+            responseCode = conn?.responseCode ?: 0
         }
-        Logger.d(this, message = "Original URL: " + conn.url)
-        return conn.url.toString()
+        Logger.d(this, message = "Original URL: " + conn?.url)
+        return conn?.url.toString()
     } catch (e: java.lang.Exception) {
         Logger.e(this, exception = e)
     } finally {
